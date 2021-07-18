57a2f60348a9c52114d22744e781a98f3ac02350
Lorenzo <Slipknot_9>
2021-07-18 15:29:50 +0200

    Initial import

---
+++ src/main/java/ApplicationRunner.java
@@ -0,0 +1,6 @@
+public class ApplicationRunner {
+    //METHODS
+    public static void main(String[] args) {
+        System.out.println("GIOCHIAMO A QUENTIN");
+    }
+}
---
+++ src/test/java/GenericTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GenericTest {
+    //METHODS
+    @Test
+    public void test() {
+        assertEquals(14, 14);
+    }
+}
