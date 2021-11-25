85dc6d8d505ae52f687e95881959ea3bb6f95f93
LemmeeD <fabbio.dev@outlook.com>
2021-11-25 23:11:48 +0100

    Fixed the stamp methods (in class BoardShellPrinter) that were wrong.

--- src/main/java/gui/BoardShellPrinter.java
+++ src/main/java/gui/BoardShellPrinter.java
@@ -34,12 +34,12 @@ public class BoardShellPrinter {
     private String stamp() {
         String result = "";
         result = result.concat(String.format("%24s%s%s%n", "-".repeat(19), " BLACK LINE ", "-".repeat(18)));
-        for (int y = 0; y < 2*this.board.getDIMENSION() - 1; y++) {
-            String borderString = IntUtils.mapBorderChar(y);
-            if ((y % 2) == 0) {
-                result = result.concat(String.format("%4s%2s", borderString, this.board.getMatrix()[0][y/2].getSymbol()));
-                for (int x = 1; x < this.board.getDIMENSION(); x++) {
-                    result = result.concat(String.format("%4s", " - "+this.board.getMatrix()[x][y/2].getSymbol()));
+        for (int r = 0; r < 2*this.board.getDIMENSION() - 1; r++) {
+            String borderString = IntUtils.mapBorderChar(r);
+            if ((r % 2) == 0) {
+                result = result.concat(String.format("%4s%2s", borderString, this.board.getMatrix()[r/2][0].getSymbol()));
+                for (int c = 1; c < this.board.getDIMENSION(); c++) {
+                    result = result.concat(String.format("%4s", " - "+this.board.getMatrix()[r/2][c].getSymbol()));
                 }
                 result = result.concat(String.format("%2s%n", borderString));
             }
@@ -55,13 +55,13 @@ public class BoardShellPrinter {
         String result = "";
         result = result.concat(String.format("%7s%4s%4s%4s%4s%4s%4s%4s%4s%4s%5s%4s%4s%n%n", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"));
         result = result.concat(String.format("%25s%s%s%n", "-".repeat(19), " BLACK LINE ", "-".repeat(18)));
-        for (int y = 0; y < 2*this.board.getDIMENSION() - 1; y++) {
-            String borderString = IntUtils.mapBorderChar(y);
-            String legendString = IntUtils.mapLegendChar(y);
-            if ((y % 2) == 0) {
-                result = result.concat(String.format("%s%4s%2s", legendString, borderString, this.board.getMatrix()[0][y/2].getSymbol()));
-                for (int x = 1; x < this.board.getDIMENSION(); x++) {
-                    result = result.concat(String.format("%4s", " - "+this.board.getMatrix()[x][y/2].getSymbol()));
+        for (int r = 0; r < 2*this.board.getDIMENSION() - 1; r++) {
+            String borderString = IntUtils.mapBorderChar(r);
+            String legendString = IntUtils.mapLegendChar(r);
+            if ((r % 2) == 0) {
+                result = result.concat(String.format("%s%4s%2s", legendString, borderString, this.board.getMatrix()[r/2][0].getSymbol()));
+                for (int c = 1; c < this.board.getDIMENSION(); c++) {
+                    result = result.concat(String.format("%4s", " - "+this.board.getMatrix()[r/2][c].getSymbol()));
                 }
                 result = result.concat(String.format("%2s%4s%n", borderString, legendString));
             }
