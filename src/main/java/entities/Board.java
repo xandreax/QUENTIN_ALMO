9e68e733859808001cd55f0549d1d698ea73bab2
Michele <micheledalcin4@gmail.com>
2021-11-26 20:18:33 +0100

    Implemented equals method in Board class and fixed the TerritoriesTest

--- src/main/java/entities/Board.java
+++ src/main/java/entities/Board.java
@@ -1,8 +1,11 @@
 package entities;
 
 import gui.BoardShellPrinter;
+
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 public class Board {
     //FIELDS
