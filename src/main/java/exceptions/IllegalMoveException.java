07bfdb6e3caa1430a60ca4ba961b6a7883272f83
andreamjc <xandreaxbora@gmail.com>
2022-01-24 16:18:28 +0100

    Refactoring: deleted field message in the exceptions

--- src/main/java/exceptions/IllegalMoveException.java
+++ src/main/java/exceptions/IllegalMoveException.java
@@ -1,11 +1,9 @@
 package exceptions;
 
 public class IllegalMoveException extends Exception{
-    //FIELDS
-    private final String message;
 
     public IllegalMoveException(String message) {
-        this.message = message;
+        super(message);
     }
 
 }
--- src/main/java/exceptions/InvalidCoordinateException.java
+++ src/main/java/exceptions/InvalidCoordinateException.java
@@ -1,15 +1,13 @@
 package exceptions;
 
 public class InvalidCoordinateException extends Exception {
-    //FIELDS
-    private final String message;
 
     //CONSTRUCTORS
     public InvalidCoordinateException() {
-        this.message = "The inserted coordinate is not valid, please use a valid one";
+        super("The inserted coordinate is not valid, please use a valid one");
     }
 
     public InvalidCoordinateException(String message) {
-        this.message = message;
+        super(message);
     }
 }
